947fdac0e8eb2974d20ff795c905c988512196c4
Emile <emile.swarts123@gmail.com>
2012-09-14 15:34:23 +0100

    uninstall vim

---
+++ uninstall_vim_manually.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rm -rf /usr/local/share/vim/vim61
+rm -rf /usr/local/bin/eview
+rm -rf /usr/local/bin/evim
+rm -rf /usr/local/bin/ex
+rm -rf /usr/local/bin/gview
+rm -rf /usr/local/bin/gvim
+rm -rf /usr/local/bin/gvim
+rm -rf /usr/local/bin/gvimdiff
+rm -rf /usr/local/bin/rgview
+rm -rf /usr/local/bin/rgvim
+rm -rf /usr/local/bin/rview
+rm -rf /usr/local/bin/rvim
+rm -rf /usr/local/bin/rvim
+rm -rf /usr/local/bin/view
+rm -rf /usr/local/bin/vim
+rm -rf /usr/local/bin/vimdiff
+rm -rf /usr/local/bin/vimtutor
+rm -rf /usr/local/bin/xxd
+rm -rf /usr/local/man/man1/eview.1
+rm -rf /usr/local/man/man1/evim.1
+rm -rf /usr/local/man/man1/ex.1
+rm -rf /usr/local/man/man1/gview.1
+rm -rf /usr/local/man/man1/gvim.1
+rm -rf /usr/local/man/man1/gvimdiff.1
+rm -rf /usr/local/man/man1/rgview.1
+rm -rf /usr/local/man/man1/rgvim.1
+rm -rf /usr/local/man/man1/rview.1
+rm -rf /usr/local/man/man1/rvim.1
+rm -rf /usr/local/man/man1/view.1
+rm -rf /usr/local/man/man1/vim.1
+rm -rf /usr/local/man/man1/vimdiff.1
+rm -rf /usr/local/man/man1/vimtutor.1
+rm -rf /usr/local/man/man1/xxd.1
